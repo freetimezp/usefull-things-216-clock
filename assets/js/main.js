874d7464bff70872ec37d452648960884e4429f2
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-07-15 23:36:14 +0300

    05. usefull-things-216-clock. Timer animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,28 @@ setInterval(() => {
     hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
-}); //can show 1
+
+    let hours = document.querySelector("#hours");
+    let minutes = document.querySelector("#minutes");
+    let seconds = document.querySelector("#seconds");
+    let ampm = document.querySelector("#ampm");
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    let am = h >= 12 ? "PM" : "AM";
+
+    if (h > 12) {
+        h = h - 12;
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
+});
